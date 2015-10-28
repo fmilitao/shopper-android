7269d1775b3f6947a983b051e63cca123cf27f3b
Filipe <fm2097ad@gmail.com>
2015-10-28 21:59:59 +0000

    fixes action of  action bar home button to be same as back button press

--- app/src/main/java/pt/blah/shopper/ProductsActivity.java
+++ app/src/main/java/pt/blah/shopper/ProductsActivity.java
@@ -1,7 +1,10 @@
 package pt.blah.shopper;
 
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
 
 public class ProductsActivity extends AppCompatActivity {
 
@@ -16,6 +19,16 @@ public class ProductsActivity extends AppCompatActivity {
                     .add(R.id.product_container, new ProductsFragment())
                     .commit();
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
